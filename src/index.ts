14c4e442b35e340592eab45585813ab233326ed0
shenhongdou <shenhongdo@126.com>
2022-09-09 15:01:33 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import '/public/iconfont_20220823.js';
+
 export { default as Panel } from './Panel';
 export { default as ResizableColumnTable } from './ResizableColumnTable';
 export { default as Layout } from './Layout';
