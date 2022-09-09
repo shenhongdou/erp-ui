002d19fc8048e052b606f16e7cc56dcf799dc8f2
shenhongdou <shenhongdo@126.com>
2022-09-09 13:34:38 +0800

    save

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as Panel } from './Panel';
 export { default as ResizableColumnTable } from './resizableColumnTable';
-export { default as Layout } from './Layout';
+export { default as Layout } from './layout';
 export { default as BpmTodoList } from './BpmTodoList';
