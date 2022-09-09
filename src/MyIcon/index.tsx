26d428fc91390b1bfa0e4d2d6f4814c5f2683dd9
shenhongdou <shenhongdo@126.com>
2022-09-09 14:16:30 +0800

    修复iconfont静态资源路径

--- src/MyIcon/index.tsx
+++ src/MyIcon/index.tsx
@@ -3,7 +3,7 @@ import { createFromIconfontCN } from '@ant-design/icons';
 import { IconFontProps } from '@ant-design/icons/lib/components/IconFont';
 
 const IconFont = createFromIconfontCN({
-  scriptUrl: '/iconfont_20220823.js',
+  scriptUrl: '/erp-ui/iconfont_20220823.js',
 });
 
 const MyIcon: React.FC<IconFontProps> = (props) => {
