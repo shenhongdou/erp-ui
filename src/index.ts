907a00f6c9120fc12202a212aa4abd703dc21df9
shenhongdou <hongdou.shen@sayweee.com>
2024-07-25 10:28:22 +0800

    add space view

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import '../public/iconfont_20220823.js';
+import '../public/iconfont_20240725.js';
 
 export { default as Panel } from './Panel';
 export { default as ResizableColumnTable } from './ResizableColumnTable';
 export { default as Layout } from './Layout';
 export { default as BpmTodoList } from './BpmTodoList';
 export { default as BpmMessage } from './BpmMessage';
+export { default as SpaceView } from './SpaceView';
---
+++ src/types/global.ts
@@ -0,0 +1,6 @@
+export enum ENV {
+  TB1 = 'tb1',
+  DEV = 'dev',
+  PROD = 'prod',
+  PRO = 'pro',
+}
